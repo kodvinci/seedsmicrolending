2458023deee350da0ce2c5b2eb9f6225c7876e89
Leonard <dranoel.bravo@gmail.com>
2012-10-17 22:39:29 -0400

    added custom FurnitureViewController

---
+++ Classes/FurnitureViewController.m
@@ -0,0 +1,43 @@
+//
+//  FurnitureViewController.m
+//  Microlending
+//
+//  Created by guest user on 10/17/12.
+//
+//
+
+#import "FurnitureViewController.h"
+
+@interface FurnitureViewController ()
+
+@end
+
+@implementation FurnitureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
