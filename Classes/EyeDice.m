14451c92cdc12bce855b0e48cd26c03c65d1e776
Benjamin Mozenter <Mozenterb@gmail.com>
2012-11-20 22:26:26 -0500

    Updated Eyes and Hair to use plists

--- Classes/EyeDice.m
+++ Classes/EyeDice.m
@@ -19,99 +19,18 @@
     return self;
 }
 
--(NSString *) eyesFromDadsEyes:(NSString *) eye andMomsEyes: (NSString *) eyes2;
+-(NSString *) eyesFromDadsEyes:(NSString *) eye andMomsEyes: (NSString *) eye2;
     {
-        NSString * eyeColor =@"Light Brown";
-        if([eye isEqualToString: @"Light Brown"] && [eyes2 isEqualToString:@"Light Brown"]){
-            eyeColor = @"Red";
-        }
-        else if([eye isEqualToString: @"Dark Brown"] && [eyes2 isEqualToString:@"Dark Brown"]){
-            eyeColor = @"Dark Brown";
-        }
-        else if([eye isEqualToString: @"Green"] && [eyes2 isEqualToString:@"Green"]){
-            eyeColor = @"Green";
-        }
-        else if([eye isEqualToString: @"Blue"] && [eyes2 isEqualToString:@"Blue"]){
-            eyeColor = @"Blue";
-        }
-        else if([eye isEqualToString: @"Grey"] && [eyes2 isEqualToString:@"Grey"]){
-            eyeColor = @"Grey";
-        }
-        else if([eye isEqualToString: @"Red"] && [eyes2 isEqualToString:@"Red"]){
-            eyeColor = @"Red";
-        }
-        else if([eye isEqualToString: @"Yellow"] && [eyes2 isEqualToString:@"Yellow"]){
-            eyeColor = @"Yellow";
+        NSString* plistPath =[[NSBundle mainBundle] pathForResource:@"EyeColors" ofType:@"plist"];
+        NSDictionary *eyesDictionary = [NSDictionary dictionaryWithContentsOfFile: plistPath];
+        if( [eye compare: eye2] == NSOrderedSame || [eye compare: eye2]==NSOrderedAscending ){
+            eye = [eye stringByAppendingString: eye2];
         }
         else{
-            NSString *combinedEyeColor = [eye stringByAppendingString: eyes2];
-            if( [self doesString: combinedEyeColor containOne: @"Light Brown" andTwo: @"Dark Brown" ]){
-                eyeColor = @"Light Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Light Brown" andTwo: @"Red" ]){
-                eyeColor = @"Light Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Yellow" andTwo: @"Red" ]){
-                eyeColor = @"Light Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Light Brown" andTwo: @"Yellow" ]){
-                eyeColor = @"Light Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Light Brown" andTwo: @"Blue" ]){
-                eyeColor = @"Dark Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Dark Brown" andTwo: @"Green" ]){
-                eyeColor = @"Dark Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Dark Brown" andTwo: @"Red" ]){
-                eyeColor = @"Dark Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Dark Brown" andTwo: @"Yellow" ]){
-                eyeColor = @"Dark Brown";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Dark Brown" andTwo: @"Grey" ]){
-                eyeColor = @"Green";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Red" andTwo: @"Blue" ]){
-                eyeColor = @"Green";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Yellow" andTwo: @"Blue" ]){
-                eyeColor = @"Green";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Grey" andTwo: @"Blue" ]){
-                eyeColor = @"Blue";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Green" andTwo: @"Yellow" ]){
-                eyeColor = @"Blue";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Grey" andTwo: @"Light Brown" ]){
-                eyeColor = @"Blue";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Green" andTwo: @"Blue" ]){
-                eyeColor = @"Grey";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Grey" andTwo: @"Red" ]){
-                eyeColor = @"Red";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Green" andTwo: @"Light Brown" ]){
-                eyeColor = @"Red";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Dark Brown" andTwo: @"Blue" ]){
-                eyeColor = @"Red";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Grey" andTwo: @"Green" ]){
-                eyeColor = @"Yellow";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Red" andTwo: @"Green" ]){
-                eyeColor = @"Yellow";
-            }
-            else if( [self doesString: combinedEyeColor containOne: @"Grey" andTwo: @"Yellow" ]){
-                eyeColor = @"Yellow";
-            }
-
-
-            
-
+            eye = [eye2 stringByAppendingString: eye];
         }
+        return [eyesDictionary valueForKey: eye];
+        
     }
+
 @end
--- Classes/HairColorDice.m
+++ Classes/HairColorDice.m
@@ -20,149 +20,17 @@
     return self;
 }
 -(NSString *) hairColorFromDad:(NSString *) hair andMom: (NSString *) hair2{
-    NSString * hairColor =@"Red";
-    if([hair isEqualToString: @"Red"] && [hair2 isEqualToString:@"Red"]){
-        hairColor = @"Red";
+    NSString* plistPath =[[NSBundle mainBundle] pathForResource:@"HairColors" ofType:@"plist"];
+    NSDictionary *hairDictionary = [NSDictionary dictionaryWithContentsOfFile: plistPath];
+    if( [hair compare: hair2] == NSOrderedSame || [hair compare: hair2]==NSOrderedAscending ){
+        hair = [hair stringByAppendingString:hair2];
     }
-    else if([hair isEqualToString: @"Pink"] && [hair2 isEqualToString:@"Pink"]){
-        hairColor = @"Pink";
-    }
-    else if([hair isEqualToString: @"Orange"] && [hair2 isEqualToString:@"Orange"]){
-        hairColor = @"Orange";
-    }
-    else if([hair isEqualToString: @"White"] && [hair2 isEqualToString:@"White"]){
-        hairColor = @"White";
-    }
-    else if([hair isEqualToString: @"Yellow"] && [hair2 isEqualToString:@"Yellow"]){
-        hairColor = @"Yellow";
-    }
-    else if([hair isEqualToString: @"Violet"] && [hair2 isEqualToString:@"Violet"]){
-        hairColor = @"Violet";
-    }
-    else if([hair isEqualToString: @"Purple"] && [hair2 isEqualToString:@"Purple"]){
-        hairColor = @"Purple";
-    }
-    else if([hair isEqualToString: @"Turquoise"] && [hair2 isEqualToString:@"Turquoise"]){
-        hairColor = @"Turquoise";
-    }
-    else if([hair isEqualToString: @"Black"] && [hair2 isEqualToString:@"Black"]){
-        hairColor = @"Black";
-    }
-    
     else{
-        NSString *combinedHairColor = [hair stringByAppendingString: hair2];
-        if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Violet" ]){
-            hairColor = @"Red";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Red" ]){
-            hairColor = @"Red";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Pink" andTwo: @"Red" ]){
-            hairColor = @"Red";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Pink" andTwo: @"Violet" ]){
-            hairColor = @"Red";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Red" andTwo: @"Violet" ]){
-            hairColor = @"Red";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Pink" andTwo: @"Turquoise" ]){
-            hairColor = @"Pink";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Red" ]){
-            hairColor = @"Pink";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Violet" ]){
-            hairColor = @"Pink";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Red" andTwo: @"Yellow" ]){
-            hairColor = @"Orange";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Pink" andTwo: @"Yellow" ]){
-            hairColor = @"Orange";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Black" ]){
-            hairColor = @"Orange";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Yellow" ]){
-            hairColor = @"White";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Pink" ]){
-            hairColor = @"White";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Black" andTwo: @"Pink" ]){
-            hairColor = @"White";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Turquoise" ]){
-            hairColor = @"White";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"White" andTwo: @"Purple" ]){
-            hairColor = @"White";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Yellow" ]){
-            hairColor = @"Yellow";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Pink" ]){
-            hairColor = @"Yellow";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"White" ]){
-            hairColor = @"Yellow";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Turquoise" andTwo: @"Yellow" ]){
-            hairColor = @"Yellow";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Black" andTwo: @"Yellow" ]){
-            hairColor = @"Yellow";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Purple" ]){
-            hairColor = @"Violet";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Red" andTwo: @"Purple" ]){
-            hairColor = @"Violet";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Orange" andTwo: @"Turquoise" ]){
-            hairColor = @"Violet";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Turquoise" andTwo: @"Red" ]){
-            hairColor = @"Violet";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Black" andTwo: @"Purple" ]){
-            hairColor = @"Purple";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Violet" andTwo: @"Purple" ]){
-            hairColor = @"Purple";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Black" andTwo: @"White" ]){
-            hairColor = @"Purple";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Violet" andTwo: @"Turquiose" ]){
-            hairColor = @"Purple";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Turquoise" andTwo: @"Purple" ]){
-            hairColor = @"Turquiose";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Turquoise" andTwo: @"Purple" ]){
-            hairColor = @"Turquiose";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Yellow" andTwo: @"Purple" ]){
-            hairColor = @"Turquiose";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Pink" andTwo: @"Purple" ]){
-            hairColor = @"Turquiose";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Yellow" andTwo: @"Violet" ]){
-            hairColor = @"Turquiose";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Turquoise" andTwo: @"Black" ]){
-            hairColor = @"Black";
-        }
-        else if( [self doesString: combinedHairColor containOne: @"Red" andTwo: @"Black" ]){
-            hairColor = @"Black";
-        }
-        
+        hair = [hair2 stringByAppendingString: hair];
     }
-    return hairColor;
+    return [hairDictionary valueForKey: hair];
     
 }
+
 
 @end
